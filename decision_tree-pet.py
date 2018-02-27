b3ffb46d0edf54ef2b168019cea70a417a0f23de
Peter Ahueze <peterahueze@gmail.com>
2018-02-27 16:42:38 +0100

    algorithms

--- decision_tree-pet.py
+++ decision_tree-pet.py
@@ -10,9 +10,9 @@ from features import target # getting the target
 
 data = pd.read_csv('lending-club-data.csv',low_memory=False)
 data['safe_loans'] = data['bad_loans'].apply((lambda x: +1 if x== 0 else -1))
-ddata = data.dropna() #dropping data wih null values
-loans = ddata[features +[target]]
-
+#ddata = data.dropna() #dropping data wih null values
+loans = data[features +[target]]
+#print loans.shape
 
 cats = []
 ccols = []
@@ -20,35 +20,27 @@ for c,d in zip(loans.columns, loans.dtypes):
   if d == object:
     cats.append(c)
 
-for cat in cats:
-  a = loans.groupby(cats)[cats].count()
-  ccols = ccols + a.index.tolist()
 
 # one hot encoding....
 print 'Performing One hot encoding...'
 cat_loans = loans[cats]
-dict_loans = cat_loans.T.to_dict().values()
+cloans = pd.get_dummies(cat_loans,prefix='feats_')
 
-vectorizer = DictVectorizer(sparse=True)
-mat_loans = vectorizer.fit_transform(dict_loans)
-
-newloans = pd.DataFrame(mat_loans, columns=ccols)
 nloans = loans.drop(cats, axis=1)
+newloans = cloans.join(nloans)
 
-newloans = newloans.join(nloans)
-
-
-feats = newloans[features]
+feats = newloans.drop(target,axis=1)
 tars = newloans[target]
 
 feat_train, feat_test, tar_train, tar_test =train_test_split(feats, tars, test_size=.3)
 
 # running the Decision tree classifier
+print; print 'Running the Decision Tree Classifier'
 classifier = DecisionTreeClassifier()
-dtree = classifier.fit(feats_train, tar_train)
+dtree = classifier.fit(feat_train, tar_train)
 
 # performing predictions
-predictions = dtree.prediction(feats_test)
+predictions = dtree.predict(feat_test)
 
 #running the metrics
 
@@ -56,5 +48,5 @@ print 'Accuracy Score =',
 print accuracy_score(predictions, tar_test)
 print
 print 'Confusion Matrix='
-print consuion_matrix(predictions, tar_test)
+print confusion_matrix(predictions, tar_test)
 
